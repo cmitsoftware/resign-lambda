a9520fa54f7abf9b878b963248168a9a231aa18f
michele <michele.cervini@gmail.com>
2019-01-28 22:03:27 +0100

    generate link handler

---
+++ src/main/java/org/resign/backend/GenerateImageLinkHandler.java
@@ -0,0 +1,72 @@
+	package org.resign.backend;
+	
+	import java.net.URL;
+import java.util.Date;
+	
+	import org.resign.backend.domain.Resource;
+	import org.resign.backend.gateway.ApiGatewayProxyResponse;
+	import org.resign.backend.gateway.ApiGatewayRequest;
+
+import com.amazonaws.AmazonServiceException;
+import com.amazonaws.HttpMethod;
+import com.amazonaws.SdkClientException;
+import com.amazonaws.auth.DefaultAWSCredentialsProviderChain;
+import com.amazonaws.auth.profile.ProfileCredentialsProvider;
+import com.amazonaws.services.lambda.runtime.Context;
+	import com.amazonaws.services.lambda.runtime.RequestHandler;
+import com.amazonaws.services.s3.AmazonS3;
+import com.amazonaws.services.s3.AmazonS3ClientBuilder;
+import com.amazonaws.services.s3.model.GeneratePresignedUrlRequest;
+import com.fasterxml.jackson.databind.ObjectMapper;
+	
+	public class GenerateImageLinkHandler implements RequestHandler<ApiGatewayRequest, ApiGatewayProxyResponse> {
+	
+		@Override
+		public ApiGatewayProxyResponse handleRequest(ApiGatewayRequest request, Context context) {
+	
+			context.getLogger().log("Request: " + request.toString());
+	
+			String clientRegion = "eu-west-3";
+			String bucketName = "resign-test-resource-images";
+			String objectKey = request.getQueryStringParameters().get("key");
+			context.getLogger().log("Generating presigned url for " + objectKey);
+			
+			try {
+				AmazonS3 s3Client = AmazonS3ClientBuilder.standard()
+	                    .withRegion(clientRegion)
+//	                    .withCredentials(new ProfileCredentialsProvider())
+	                    .withCredentials(DefaultAWSCredentialsProviderChain.getInstance())
+	                    .build();
+				
+				// Set the presigned URL to expire after one hour.
+				java.util.Date expiration = new java.util.Date();
+				long expTimeMillis = expiration.getTime();
+				expTimeMillis += 1000 * 60;
+				expiration.setTime(expTimeMillis);
+		
+				// Generate the presigned URL.
+				System.out.println("Generating pre-signed URL.");
+				GeneratePresignedUrlRequest generatePresignedUrlRequest = 
+						new GeneratePresignedUrlRequest(bucketName, objectKey)
+						.withMethod(HttpMethod.POST)
+						.withExpiration(expiration);
+				URL url = s3Client.generatePresignedUrl(generatePresignedUrlRequest);
+		
+				context.getLogger().log("Pre-Signed URL: " + url.toString());
+		
+				ApiGatewayProxyResponse response = new ApiGatewayProxyResponse(200, null, url.toString());
+				return response;
+			}
+			catch(AmazonServiceException e) {
+				// The call was transmitted successfully, but Amazon S3 couldn't process 
+				// it, so it returned an error response.
+				e.printStackTrace();
+			}
+			catch(SdkClientException e) {
+				// Amazon S3 couldn't be contacted for a response, or the client
+				// couldn't parse the response from Amazon S3.
+				e.printStackTrace();
+			}
+			return new ApiGatewayProxyResponse(500, null, "An error occurred");
+		}
+	}
--- src/main/java/org/resign/backend/domain/User.java
+++ src/main/java/org/resign/backend/domain/User.java
@@ -1,14 +1,61 @@
 package org.resign.backend.domain;
 
+import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBAttribute;
+import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBHashKey;
 import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBTable;
+import com.fasterxml.jackson.annotation.JsonInclude;
 
-@DynamoDBTable(tableName = "resource")
+@DynamoDBTable(tableName = "user")
 public class User extends ApiResponse {
 	
+	@JsonInclude(JsonInclude.Include.NON_NULL)
 	private String userId;
+	@JsonInclude(JsonInclude.Include.NON_NULL)
 	private String email;
+	@JsonInclude(JsonInclude.Include.NON_NULL)
 	private String ts;
+	@JsonInclude(JsonInclude.Include.NON_NULL)
 	private String name;
+	@JsonInclude(JsonInclude.Include.NON_NULL)
 	private String surname;
-
+	
+	@DynamoDBHashKey
+	public String getUserId() {
+		return userId;
+	}
+	public void setUserId(String userId) {
+		this.userId = userId;
+	}
+	
+	@DynamoDBAttribute
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	
+	@DynamoDBAttribute
+	public String getTs() {
+		return ts;
+	}
+	public void setTs(String ts) {
+		this.ts = ts;
+	}
+	
+	@DynamoDBAttribute
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	@DynamoDBAttribute
+	public String getSurname() {
+		return surname;
+	}
+	public void setSurname(String surname) {
+		this.surname = surname;
+	}
 }
